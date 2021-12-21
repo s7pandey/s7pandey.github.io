f6216cb77b71992882b3934a95cda693806f4432
Shashwat Pandey <s7pandey@edu.uwaterloo.ca>
2021-12-21 16:40:45 -0500

    Fixing some lighting

--- data/content.js
+++ data/content.js
@@ -1,4 +1,49 @@
 data = {
-    "nodes": [],
+    "nodes": [{
+            "id": "My Favorite Murder - 305",
+            "group": "My Favorite Murder",
+            "details": {
+                "episode": "305",
+                "description": "Amanda Milan and Toronto Circus Riot of 1855"
+            },
+            "tags": ["podcast", "crime"]
+        },
+        {
+            "id": "Radiolab - Return of Alpha Gal",
+            "group": "Radiolab",
+            "details": {
+                "episode": "Return of Alpha Gal - December 10",
+                "description": "Replay of Alpha Gal (tick-borne disease making humans allergic to red meat) & updates"
+            },
+            "tags": ["podcast", "documentary"]
+        },
+        {
+            "id": "Freaknomics Radio - 486",
+            "group": "Freaknomics Radio",
+            "details": {
+                "episode": "486",
+                "description": "Finishing 3-part dive into Art Market, capping off with NFTs and democrotization of Art"
+            },
+            "tags": ["podcast", "documentary"]
+        },
+        {
+            "id": "Freaknomics Radio - 485",
+            "group": "Freaknomics Radio",
+            "details": {
+                "episode": "485",
+                "description": "Second episode of dive into Art Market - How the art market works today for Artists, speak to Tom Sachs"
+            },
+            "tags": ["podcast", "documentary"]
+        },
+        {
+            "id": "Freaknomics Radio - 484",
+            "group": "Freaknomics Radio",
+            "details": {
+                "episode": "484",
+                "description": "First Episode in dive into Art Market, overview of the art market"
+            },
+            "tags": ["podcast", "documentary"]
+        }
+    ],
     "links": [],
 }
